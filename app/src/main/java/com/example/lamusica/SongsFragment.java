442cd949640f36aded9787375012cf0dfff42c15
hauxsho <hauxsho@gmail.com>
2020-06-21 15:08:30 +0530

    Second

---
+++ app/src/main/java/com/example/lamusica/SongsFragment.java
@@ -0,0 +1,27 @@
+package com.example.lamusica;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SongsFragment extends Fragment {
+
+    public SongsFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_songs, container, false);
+        return view;
+    }
+
+}
